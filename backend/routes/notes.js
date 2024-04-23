c68b6ed0941eb24709d12b74146ded0d19230ca6
Kesar25 <117337016+Kesar25@users.noreply.github.com>
2024-04-23 14:21:35 +0530

    Updating a note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -42,6 +42,21 @@ body('description', 'Description should be atleast 3 characters long').isLength(
 
 })
 
+router.put("/updatenote/:id", fetchUser, async(req,res)=>{
+    const {title, description, tag}= req.body;
+    const newNote={};
 
+    if(title) {newNote.title=title};
+    if(description) {newNote.description=description};
+    if(tag) {newNote.tag=tag};
+
+    let note=await Note.findById(req.params.id);
+    if(!note) {return res.status(404).send("Not Found")};
+
+    if(note.user.toString()!==req.user.id){ return res.status(401).send("Not Allowed to update")};
+
+    note= await Note.findByIdAndUpdate(req.params.id, {$set:newNote}, {new:true});
+    res.json(note)
+})
 
 module.exports=router;

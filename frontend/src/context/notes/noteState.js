727567f342e69983beda5a2bd450c1d1b9c91975
Kesar25 <117337016+Kesar25@users.noreply.github.com>
2024-06-23 19:41:55 +0530

    Modal for edit a note

--- frontend/src/context/notes/noteState.js
+++ frontend/src/context/notes/noteState.js
@@ -49,16 +49,25 @@ const NoteState = (props) => {
     }
 
 
-    const deleteNote = (id) => {
-      // TODO: API Call
+    const deleteNote =async (id) => {
+      const response = await fetch(`${host}/api/v1/notes/deletenote/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjYyNjI3NWMzNGU1ZjU2MGQ2M2UwZTdlIn0sImlhdCI6MTcxMzgwMzkwM30.Lzl0yG3XlAp8FRz6FUXuNxqsF_1XIHS6-CfsJKjdfqg"
+        }
+      });
+      const json =await response.json();
+      console.log(json);
+  
       console.log("Deleting the note with id" + id);
       const newNotes = notes.filter((note) => { return note._id !== id })
-      setNotes(newNotes)
+      setNotes(newNotes);
     }
     // Edit a Note
     const editNote = async (id, title, description, tag) => {
       // API Call 
-      const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
+      const response = await fetch(`${host}/api/v1/notes/updatenote/${id}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -79,7 +88,7 @@ const NoteState = (props) => {
 
     const [notes, setNotes] = useState(noteInitial);
     return (
-        <NoteContext.Provider value={{notes,addNote,deleteNote,editNote}}>
+        <NoteContext.Provider value={{notes,addNote,deleteNote,editNote,getNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
